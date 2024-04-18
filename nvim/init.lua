3dc5c5f397339fb0eaa76b6ebc6a01bb0f6ae0bd
ducvu <ducvu973xd@gmail.com>
2024-04-18 16:08:17 +0700

    updated nvim colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -309,7 +309,7 @@ require('lazy').setup {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd [[colorscheme base16-gruvbox-dark-hard]]
+      vim.cmd 'colorscheme base16-gruvbox-dark-hard'
     end,
   },
 
