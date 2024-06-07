669dcf8d024e5198bd2c3c748559a7dd9788f546
ducvu <ducvu973xd@gmail.com>
2024-06-07 22:20:38 +0700

    nvim, vscode: updated keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,7 +27,6 @@ vim.opt.termguicolors = false
 vim.opt.cursorline = false
 
 -- keymaps
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
