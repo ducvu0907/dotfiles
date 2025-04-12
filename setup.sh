880ce3c40a63a5a608729429546d77ceab64e502
ducvu0907 <ducvu973xd@gmail.com>
2025-04-12 16:38:17 +0700

    update intellij vscode and bash config

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#/usr/bin/bash
+DOTFILES=~/dotfiles
+CONFIG=~/.config
+
+ln -sf $DOTFILES/alacritty $CONFIG/alacritty
+ln -sf $DOTFILES/nvim $CONFIG/nvim
+ln -sf $DOTFILES/kitty $CONFIG/kitty
+ln -sf $DOTFILES/tmux $CONFIG/tmux
+ln -sf $DOTFILES/bash/bashrc ~/.bashrc
+
+source ~/.bashrc
+
